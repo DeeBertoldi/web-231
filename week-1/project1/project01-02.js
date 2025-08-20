66ea3569df5643b473ef869b880170a6110b8b34
Daniella Bertoldi <bertoldi.daniella@gmail.com>
2025-08-19 21:32:37 -0400

    edited project01

---
+++ week-1/project1/project01-02.js
@@ -0,0 +1,8 @@
+/*
+  Author: Daniella Bertoldi
+  Date: 08/19/2025
+  File: project01-02.js
+*/
+
+let service1Name = "Basic", service2Name = "Express", service3Name = "Extreme", service4Name = "Ultimate";
+let service1Speed = "50 Mbps", service2Speed = "100 Mbps", service3Speed = "500 Mbps", service4Speed = "1 Gig";
