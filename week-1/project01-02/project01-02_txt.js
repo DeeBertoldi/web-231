a22b23783c1be734396d20e714638636f10b5923
Daniella Bertoldi <bertoldi.daniella@gmail.com>
2025-09-01 23:16:10 -0400

    92a9f11ce81b7f5b190aa438b5aa9d9d8ac92f62

---
+++ week-1/project01-02/project01-02_txt.js
@@ -0,0 +1,22 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Daniella Bertoldi
+    Date:   08/23/2025
+
+    Filename: project01-02.js
+
+    fixing mistake i had made
+*/
+
+// define variables
+let service1Name = "Basic",
+    service2Name = "Express",
+    service3Name = "Extreme",
+    service4Name = "Ultimate";
+
+let service1Speed = "0 Mbps",
+    service2Speed = "100 Mbps",
+    service3Speed = "500 Mbps",
+    service4Speed = "1 Gig";
--- week-1/project1/project01-02.js
+++
@@ -1,9 +0,0 @@
-/*
-  Author: Daniella Bertoldi
-  Date: 8-19-25
-  File: project01-02.js
-*/
-
-let service1Name = "Basic", service2Name = "Express", service3Name = "Extreme", service4Name = "Ultimate";
-let service1Speed = "0 Mbps", service2Speed = "100 Mbps", service3Speed = "500 Mbps", service4Speed = "1 Gig";
-
